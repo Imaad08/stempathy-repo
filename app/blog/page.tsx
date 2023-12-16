78a5678a18b87a3100cf2f56d3372bed6e044d77
Imaad08 <imaadmuzaffer@gmail.com>
2023-12-16 11:30:12 -0800

    sanity implementation

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,6 +3,17 @@ import blogData from "@/components/Blog/blogData";
 import Breadcrumb from "@/components/Common/Breadcrumb";
 // import Posts from "@/components/Posts";
 import { Metadata } from "next";
+import Link from "next/link";
+import { Post } from "@/app/lib/interface";
+import { client } from "@/app/lib/sanity";
+
+async function getData() {
+  const query = `*[_type == "post"]`;
+
+  const data = await client.fetch(query);
+
+  return data;
+}
 
 export const metadata: Metadata = {
   title: "Blog Page | STEMpathy DNHS",
@@ -10,7 +21,8 @@ export const metadata: Metadata = {
   // other metadata
 };
 
-const Blog = () => {
+export default async function Blog() {
+  const data = (await getData()) as Post[];
   // const postMetadata = getPostMetadata();
   // const postPreviews = postMetadata.map((post) => (
   //   <PostPreview key={post.slug} {...post} />
@@ -26,6 +38,43 @@ const Blog = () => {
         <div className="container">
           <div className="-mx-4 flex flex-wrap justify-center">
           </div>
+          <div className="divide-y divide-gray-200 dark:divide-gray-700">
+      <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+  <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 mt-4">
+    All Posts
+  </h1>
+</div>
+
+<ul>
+  {data.map((post) => (
+    <li key={post._id} className="py-4">
+      <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+        <div>
+          <p className="text-base font-medium leading-6 text-teal-500 mb-2">
+            {new Date(post._createdAt).toISOString().split("T")[0]}
+          </p>
+        </div>
+
+        <Link
+          href={`/post/${post.slug.current}`}
+          prefetch
+          className="space-y-3 xl:col-span-3"
+        >
+          <div>
+            <h3 className="text-2xl font-bold leading-8 tracking-tight text-gray-900 dark:text-gray-100 mb-2">
+              {post.title}
+            </h3>
+          </div>
+
+          <p className="prose max-w-none text-gray-500 dark:text-gray-400 line-clamp-2">
+            {post.overview}
+          </p>
+        </Link>
+      </article>
+    </li>
+  ))}
+</ul>
+    </div>
 
           <div
             className="wow fadeInUp -mx-4 flex flex-wrap"
@@ -38,4 +87,3 @@ const Blog = () => {
   );
 };
 
-export default Blog;
---
+++ app/lib/sanity.ts
@@ -0,0 +1,12 @@
+import { createClient } from "next-sanity";
+
+const projectId = "mi1ta12r";
+const dataset = "production";
+const apiVersion = "2023-03-07";
+
+export const client = createClient({
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: true,
+});
