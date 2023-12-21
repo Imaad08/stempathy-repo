7141faff6c5c94b7694084ee9fbea102070bfc12
Imaad08 <imaadmuzaffer@gmail.com>
2023-12-21 11:49:40 -0800

    speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { Inter } from "next/font/google";
 import "node_modules/react-modal-video/css/modal-video.css";
 import "../styles/index.css";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({
           <Header />
           {children}
           <Analytics />
+          <SpeedInsights />
           <Footer />
           <ScrollToTop />
         </Providers>
