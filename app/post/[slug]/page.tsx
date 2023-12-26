28e8ea92ebb514a463301cd00d6ebb64b7aed984
Imaad08 <imaadmuzaffer@gmail.com>
2023-12-25 21:12:08 -0600

    small metadata change

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Post",
+  title: "Post | STEMpathy DNHS",
   description: "One of many blogs for STEMPATHY",
   // other metadata
 };
