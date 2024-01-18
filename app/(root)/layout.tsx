1a3a7d08ce3800aa2b2be2818656254f76b1ea27
Imaad08 <imaadmuzaffer@gmail.com>
2024-01-17 23:21:33 -0800

    file organization

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,7 +5,7 @@ import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
 import { Inter } from "next/font/google";
 import "node_modules/react-modal-video/css/modal-video.css";
-import "../styles/index.css";
+import "../../styles/index.css";
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
