04a83d1aa94495bb914372337ef758f270f4e3af
Imaad08 <imaadmuzaffer@gmail.com>
2024-03-11 16:06:31 -0700

    sanity file

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -2,7 +2,7 @@ import { createClient } from "next-sanity";
 
 const projectId = "mi1ta12r";
 const dataset = "production";
-const apiVersion = "2025-03-07";
+const apiVersion = "2028-03-07";
 
 export const client = createClient({
   projectId,
