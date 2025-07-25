cef7c2a287265f77b1143755cb0c8897e98a4710
Imaad08 <imaadmuzaffer@gmail.com>
2025-07-24 23:42:22 -0700

    revert

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -5,7 +5,7 @@ const Donate = () => {
     <>
       <section
         id="donate"
-        className="dark:bg-gray-dark relative z-10 overflow-hidden bg-white pb-16 pt-[120px] md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[100px] 2xl:pt-[160px] min-h-screen"
+        className="dark:bg-gray-dark relative z-10 overflow-hidden bg-white pb-16 pt-[120px] md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[100px] 2xl:pt-[160px]"
       >
         <div className="container">
           <div className="-mx-4 flex flex-wrap">
